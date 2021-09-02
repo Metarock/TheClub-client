e9c7b9e1a39a20ec1b02f3c06016757ee61be52a
Metarock <esang037@gmail.com>
2021-09-02 21:59:58 +1200

    testing it works

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,68 @@
-import React from 'react';
+import { Box, Container, Stack, VisuallyHidden, Text } from '@chakra-ui/react';
+import { chakra, useColorModeValue } from '@chakra-ui/system';
+import React, { ReactNode } from 'react';
+import { FaTwitter, FaYoutube, FaInstagram } from 'react-icons/fa';
+import { RouteComponentProps } from 'react-router';
 
-interface homeProps {
+const SocialButton = ({
+    children,
+    label,
+    href,
+}: {
+    children: ReactNode;
+    label: string;
+    href: string;
+}) => {
+    return (
+        <chakra.button
+            bg={useColorModeValue('blackAlpha.100', 'whiteAlpha.100')}
+            rounded={'full'}
+            w={8}
+            h={8}
+            cursor={'pointer'}
+            as={'a'}
+            href={href}
+            display={'inline-flex'}
+            alignItems={'center'}
+            justifyContent={'center'}
+            transition={'background 0.3s ease'}
+            _hover={{
+                bg: useColorModeValue('blackAlpha.200', 'whiteAlpha.200'),
+            }}>
+            <VisuallyHidden>{label}</VisuallyHidden>
+            {children}
+        </chakra.button>
+    );
+};
 
-}
 
-
-
-export const Home: React.FC<homeProps> = () => {
+export const Home: React.FC<RouteComponentProps> = () => {
     return (
 
-        <div>home page</div>
+        <Box
+            bg={useColorModeValue('gray.50', 'gray.900')}
+            color={useColorModeValue('gray.700', 'gray.200')}>
+            <Container
+                as={Stack}
+                maxW={'6xl'}
+                py={4}
+                direction={{ base: 'column', md: 'row' }}
+                spacing={4}
+                justify={{ base: 'center', md: 'space-between' }}
+                align={{ base: 'center', md: 'center' }}>
+                <Text>© 2020 Chakra Templates. All rights reserved</Text>
+                <Stack direction={'row'} spacing={6}>
+                    <SocialButton label={'Twitter'} href={'#'}>
+                        <FaTwitter />
+                    </SocialButton>
+                    <SocialButton label={'YouTube'} href={'#'}>
+                        <FaYoutube />
+                    </SocialButton>
+                    <SocialButton label={'Instagram'} href={'#'}>
+                        <FaInstagram />
+                    </SocialButton>
+                </Stack>
+            </Container>
+        </Box>
     );
 }
