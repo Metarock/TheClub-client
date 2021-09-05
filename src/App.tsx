869bccbe34a34fa481f256f2c2cdf7a0eb019b2a
Metarock <esang037@gmail.com>
2021-09-05 20:33:04 +1200

    jwt working sort of

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,4 @@
 import * as React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import { Footer } from "./components/Footer";
-import { Navbar } from "./components/Navbar";
-import { CreatePage } from "./pages/create-page";
-import { EditPage } from "./pages/edit/[id]";
-import { Home } from "./pages/home";
-import { Login } from "./pages/login";
-import { Register } from "./pages/register";
-import Page from "./pages/[id]";
 import { Routes } from "./Routes";
 import { setAccessToken } from "./utils/accessToken";
 interface AppProps {
