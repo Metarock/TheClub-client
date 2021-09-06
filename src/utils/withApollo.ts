ac2b7ba5472275865bf91d51ce25e6f2f9887db0
Metarock <esang037@gmail.com>
2021-09-07 11:24:36 +1200

    deploy again

--- src/utils/withApollo.ts
+++ src/utils/withApollo.ts
@@ -1,10 +1,16 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, createHttpLink, InMemoryCache, NormalizedCacheObject } from "@apollo/client";
 
+const local = localStorage.getItem("Cookie");
+console.log(local);
 
-const client = new ApolloClient({
+const link = createHttpLink({
     uri: process.env.REACT_APP_API_URL,
     credentials: "include",
+})
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
     cache: new InMemoryCache(),
+    link
 })
 
 export default client;
