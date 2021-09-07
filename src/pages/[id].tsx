9524aa995865d337a1d352407c6e5976b12ffe2f
Metarock <esang037@gmail.com>
2021-09-08 08:51:44 +1200

    changed page page

--- src/pages/[id].tsx
+++ src/pages/[id].tsx
@@ -1,9 +1,9 @@
 
+import { Image } from '@chakra-ui/image';
+import { Container, Heading, SimpleGrid } from '@chakra-ui/layout';
 import React from 'react';
-import { RouteComponentProps, useParams } from "react-router-dom"
-import { Card } from '../components/Card';
-import { Layout } from '../components/Layout';
-import { useMeQuery, usePageQuery } from '../generated/graphql';
+import { RouteComponentProps, useParams } from "react-router-dom";
+import { usePageQuery } from '../generated/graphql';
 
 const Page: React.FC<RouteComponentProps> = ({ history }) => {
     const { id }: any = useParams(); //get id
@@ -12,20 +12,19 @@ const Page: React.FC<RouteComponentProps> = ({ history }) => {
 
     const pageQuery = usePageQuery({ variables: { id: getId } });
     console.log(pageQuery);
-    const { data: meData } = useMeQuery({ fetchPolicy: "network-only" });
+    // const { data: meData } = useMeQuery({ fetchPolicy: "network-only" });
 
     const page = pageQuery.data?.page;
 
     if (page === undefined) return <p>loading</p>
     if (page === null) return <p>Page not found</p> //error 404
     return (
-        <Layout>
-            <Card
-                {...page}
-                creatorName={page.creator.clubUsername}
-                userIsOwner={!!meData?.me && meData.me?.id === page.creator.id}
-            />
-        </Layout>
+        <Container maxW={'5xl'} py={12}>
+            <SimpleGrid>
+                <Heading textTransform={'uppercase'}>{page.pageTitle}</Heading>
+                <Image mx="auto" src={page.pageimgUrl} mt={3} maxH={800} />
+            </SimpleGrid>
+        </Container>
     );
 }
 
